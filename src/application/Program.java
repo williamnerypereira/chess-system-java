cfad865f9a94a044803a26c565e5fc391e086f5c
williamnerypereira <william.nery.pereira@gmail.com>
2021-10-08 13:44:40 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,8 +5,9 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-
+		
 		ChessMatch chessMatch = new ChessMatch();
+		System.out.println();
 		UI.printBoard(chessMatch.getPieces());
 	}
 
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,16 +3,14 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-	
+
 	private char column;
 	private int row;
-	
+
 	public ChessPosition(char column, int row) {
-		
-		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
-		
 		this.column = column;
 		this.row = row;
 	}
@@ -28,14 +26,14 @@ public class ChessPosition {
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
-	
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getColumn());
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
 	}
-	
+
 	@Override
 	public String toString() {
-		return "" + column + row ;
+		return "" + column + row;
 	}
-	
+
 }
